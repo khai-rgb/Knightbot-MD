54058adfde5a7319460557b36cef119c03980b70
khai-rgb <dvdobdndnd12@gmail.com>
2025-04-20 21:22:49 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  author: 'Fred',
+  botName: "Khai's bot",
+  botOwner: 'Khairul R', // Your name
+  ownerNumber: '01123906112', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
